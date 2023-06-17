72c4ac854a2401d01560047e2c2c3b6faf02cd45
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-06-18 00:33:45 +0200

    Create Projects.tsx

---
+++ components/Projects.tsx
@@ -0,0 +1,12 @@
+import Section from "./Section";
+import SectionName from "./SectionName";
+
+const Projects = () => {
+  return (
+    <Section id="projects" className="bg-gray-100">
+      <SectionName name="Projects" />
+    </Section>
+  );
+};
+
+export default Projects;
