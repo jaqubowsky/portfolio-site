16c0ff469b26500c5ae66ec425da5075354c891e
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-06-18 00:33:34 +0200

    Create aboutMe.ts

---
+++ lib/aboutMe.ts
@@ -0,0 +1,5 @@
+export const aboutMe = `Self-taught programmer with a solid foundation in front-end development, backed by 8 months
+of intensive non-commercial hands-on experience. I possess a keen aptitude for learning,
+effortlessly grasping new concepts and technologies. Eager to further enhance my skills and
+gain practical expertise, I am seeking an opportunity as an front-end developer, where I can
+channel my passion for coding into real-world projects`;
