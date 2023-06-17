7ce40a891eccc701d82021354eccb9924daefcab
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-06-18 00:33:38 +0200

    Create Skills.tsx

---
+++ components/Skills.tsx
@@ -0,0 +1,23 @@
+const skills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "NodeJS",
+  "Next.js",
+  "Tailwind CSS",
+  "Git",
+  "Vite",
+];
+
+const Skills = () => {
+  const skillsEl = skills.map((skill) => (
+    <div key={skill}>
+      <h3>{skill}</h3>
+    </div>
+  ));
+
+  return <div className="grid grid-cols-3 gap-4">{skillsEl}</div>;
+};
+
+export default Skills;
