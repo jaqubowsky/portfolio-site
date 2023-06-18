dc5d0b327e4cf47bb49dc06fe63b0429f517d18a
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-06-18 15:04:12 +0200

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,10 +1,75 @@
+import ProjectCard from "./ProjectCard";
 import Section from "./Section";
 import SectionName from "./SectionName";
+import FakeStore from "@assets/images/fakestore.png";
+import PayTo from "@assets/images/payto.png";
+import QuizzApp from "@assets/images/quizzapp.png";
+import MovieWatchList from "@assets/images/moviewatchlist.png";
+import Portfolio from "@assets/images/portfolio.png";
+
+const projects = [
+  {
+    id: 1,
+    title: "FakeStore",
+    description:
+      "FakeStore is a fake e-commerce site built with React, Next.js and TailwindCSS.",
+    images: [FakeStore],
+    link: "https://github.com/jaqubowsky/shopping-app-v2",
+    technologies: ["React", "TypeScript", "TailwindCSS"],
+  },
+  {
+    id: 2,
+    title: "FakeStore API",
+    description:
+      "FakeStore API is a fake e-commerce API built with Node.js and Express.",
+    images: [],
+    link: "https://github.com/jaqubowsky/shopping-app-v2-api",
+    technologies: ["Node.js", "Express", "Prisma"],
+  },
+  {
+    id: 3,
+    title: "PayTo",
+    description: "App that simulates real card validation form on a page.",
+    images: [PayTo],
+    link: "https://github.com/jaqubowsky/PayTo",
+    technologies: ["JavaScript", "HTML", "SCSS"],
+  },
+  {
+    id: 4,
+    title: "Quizz App",
+    description:
+      "App that allows the user to select answers and then displays the correct results.",
+    images: [QuizzApp],
+    link: "https://github.com/jaqubowsky/quizz-app",
+    technologies: ["React", "Styled Components", "Vite"],
+  },
+  {
+    id: 5,
+    title: "Movie watchlist",
+    description: "App that allows the user to add movies to the watchlist.",
+    images: [MovieWatchList],
+    link: "https://github.com/jaqubowsky/movie-watchlist",
+    technologies: ["JavaScript", "HTML", "CSS"],
+  },
+  {
+    id: 6,
+    title: "Portfolio site",
+    description: "My portfolio site built with React, Next.js and TailwindCSS.",
+    images: [Portfolio],
+    link: "https://github.com/jaqubowsky/portfolio-site",
+    technologies: ["React", "Next.js", "TailwindCSS"],
+  },
+];
 
 const Projects = () => {
+  const projectsEl = projects.map((project) => (
+    <ProjectCard key={project.id} project={project} />
+  ));
+
   return (
-    <Section id="projects" className="bg-gray-100">
-      <SectionName name="Projects" />
+    <Section id="projects" className="bg-gray-200">
+      <SectionName>Projects</SectionName>
+      <div className="grid grid-cols-3 gap-x-16 gap-y-8">{projectsEl}</div>
     </Section>
   );
 };
