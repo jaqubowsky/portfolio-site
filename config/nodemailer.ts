2e94d9316e911250b693c497a9b277fdb6c9c8d9
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-06-20 21:09:56 +0200

    Config nodemailer

---
+++ config/nodemailer.ts
@@ -0,0 +1,15 @@
+import nodemailer from 'nodemailer';
+
+const email = process.env.EMAIL;
+const password = process.env.EMAIL_PASS;
+
+export const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  service: 'gmail',
+  auth: {
+    user: email,
+    pass: password,
+  }
+})
