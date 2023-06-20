3223f5dc8ae0799d34698c32bfd3e83ad9510d6f
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-06-20 23:12:35 +0200

    Lint

--- components/Form.tsx
+++ components/Form.tsx
@@ -24,51 +24,51 @@ const Form = () => {
   };
 
   return (
-    <form
-      onSubmit={onSubmit}
-      className="flex-col flex-container gap-6 mt-10 text-white w-full items-end"
-    >
-      <label htmlFor="subject" className="w-full">
-        <Input
-          type="text"
-          placeholder="Subject"
-          name="subject"
-          id="subject"
-          value={formValues.subject}
-          onChange={setFormValue}
-        />
-      </label>
-      <label htmlFor="name" className="w-full">
-        <Input
-          type="text"
-          placeholder="Name"
-          id="name"
-          name="name"
-          value={formValues.name}
-          onChange={setFormValue}
-        />
-      </label>
-      <label htmlFor="email" className="w-full">
-        <Input
-          type="email"
-          placeholder="Email"
-          name="email"
-          id="email"
-          value={formValues.email}
-          onChange={setFormValue}
-        />
-      </label>
-      <label htmlFor="message" className="w-full">
-        <Textarea
-          placeholder="Your message..."
-          name="message"
-          value={formValues.message}
-          id="message"
-          onChange={setFormValue}
-        />
-      </label>
-      <FormButton>Send</FormButton>
-    </form>
+      <form
+        onSubmit={onSubmit}
+        className="flex-col flex-container gap-2 mt-10 text-white w-full max-w-screen-md"
+      >
+        <label htmlFor="subject" className="w-full">
+          <Input
+            type="text"
+            placeholder="Subject"
+            name="subject"
+            id="subject"
+            value={formValues.subject}
+            onChange={setFormValue}
+          />
+        </label>
+        <label htmlFor="name" className="w-full">
+          <Input
+            type="text"
+            placeholder="Name"
+            id="name"
+            name="name"
+            value={formValues.name}
+            onChange={setFormValue}
+          />
+        </label>
+        <label htmlFor="email" className="w-full">
+          <Input
+            type="email"
+            placeholder="Email"
+            name="email"
+            id="email"
+            value={formValues.email}
+            onChange={setFormValue}
+          />
+        </label>
+        <label htmlFor="message" className="w-full">
+          <Textarea
+            placeholder="Your message..."
+            name="message"
+            value={formValues.message}
+            id="message"
+            onChange={setFormValue}
+          />
+        </label>
+        <FormButton>Send</FormButton>
+      </form>
   );
 };
 
