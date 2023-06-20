bdd8a6e5c27dcde969762dbe7baa2f751f0fa80f
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-06-20 23:12:47 +0200

    Finish footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,18 @@
-import { FC, ReactNode } from "react";
+import { FC } from "react";
+import FooterIcons from "./FooterIcons";
+import FooterScrollUp from "./FooterScrollUp";
 
-interface Footer {
-  children: ReactNode;
-}
-
-const Footer: FC<Footer> = ({ children }) => {
-  return <footer>{children}</footer>;
+const Footer: FC = () => {
+  return (
+    <footer className="bg-rose-950 w-full p-12 flex-container flex-col relative">
+      <FooterScrollUp />
+      <FooterIcons />
+      <small className="text-white">
+        Jakub Nalewajk{" "}
+        <span className="text-red-500 saturate-200 drop-shadow-lg">@2023</span>
+      </small>
+    </footer>
+  );
 };
 
 export default Footer;
