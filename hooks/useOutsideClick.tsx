acd705961db4a626d8c6d9d5d3c48e032e93ce38
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-06-22 17:06:50 +0200

    Create useOutsideClick.tsx

---
+++ hooks/useOutsideClick.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from "react";
+
+type UseOutsideClick = () => void;
+
+const useOutsideClick = (callback: UseOutsideClick) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClick = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        callback();
+      }
+    };
+
+    document.addEventListener("click", handleClick);
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, [ref, callback]);
+
+  return ref;
+};
+
+export default useOutsideClick;
